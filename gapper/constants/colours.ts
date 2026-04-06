fe4c8b677e8b728031aa47d3490b20d1a4ab4270
ljkkj7 <kianjenkins26@gmail.com>
2026-04-06 14:23:49 +0100

    Colour constants for TFL line colours

---
+++ gapper/constants/colours.ts
@@ -0,0 +1,13 @@
+export const LINE_COLOURS = {
+    'Central': '#DC241F',
+    'Northern': '#000000',
+    'Jubilee': '#A0A5A9',
+    'Piccadilly': '#003688',
+    'Victoria': '#0098D4',
+    'Bakerloo': '#B36305',
+    'District': '#00782A',
+    'Circle': '#FFD329',
+    'Metropolitan': '#9B0056',
+    'Hammersmith & City': '#F3A9BB',
+    'Waterloo & City': '#95CDBA',
+}
